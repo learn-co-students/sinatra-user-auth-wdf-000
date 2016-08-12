86b136000a2f96b9c374ff75619a3bd1e12ed40a
fidelscodes <fidelscodes@gmail.com>
2016-08-12 11:39:54 -0400

    Get all tests passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
+  # Renders homepage and Sign Up / Login links
   get '/' do
     erb :home
   end
@@ -19,6 +20,15 @@ class ApplicationController < Sinatra::Base
   # Receives POST request when user 'submits' from the signup form
   # Responsible for creating the new user, signing them in, redirecting
   post '/registrations' do
+    # @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    # @user.save]
+
+    # Create and save a new user
+    @user = User.create(params)
+
+    # Sign in the newly created user
+    session[:id] = @user.id
+
     redirect '/users/home'
   end
 
@@ -31,18 +41,26 @@ class ApplicationController < Sinatra::Base
   # Receives the POST request from 'submit' on the login form
   # Responsible for finding the user and signing them in
   post '/sessions' do
+    # Find the right user based on params' information
+    @user = User.find_by(email: params[:email], password: params[:password])
+
+    # Sign in the user
+    session[:id] = @user.id
 
     redirect '/users/home'
   end
 
   # Responsible for logging the user out (clearing the 'session')
   get '/sessions/logout' do
+    session.clear
 
     redirect '/'
   end
 
   # Renders the user's homepage
   get '/users/home' do
+    # @user = User.find_by(id: session[:id])
+    @user = User.find(session[:id])
 
     erb :'/users/home'
   end
